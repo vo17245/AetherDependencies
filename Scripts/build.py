8eec5197f069f939a7c36c1a46616961977622e1
linyingxiong <87613099+vo17245@users.noreply.github.com>
2025-12-24 11:36:26 +0800

    fix: libpng build

--- Scripts/build.py
+++ Scripts/build.py
@@ -18,9 +18,13 @@ def build_libpng(build_type:str):
     run_command(["cmake","-B",f"Build/libpng/{build_type}","-S","libpng",f"-DCMAKE_BUILD_TYPE={build_type}",
                  "-G","Visual Studio 17 2022","-A","x64",f"-DCMAKE_INSTALL_PREFIX=Packages/{build_type}",
                  "-D","CMAKE_POSITION_INDEPENDENT_CODE=ON",
+                 "-D","PNG_TOOLS=OFF",
                  "-D","PNG_TESTS=OFF",
                  "-D","PNG_STATIC=ON",
-                 "-D","CMAKE_PREFIX_PATH="+os.path.abspath(f"Packages/{build_type}")])
+                 "-D","CMAKE_PREFIX_PATH="+os.path.abspath(f"Packages/{build_type}"),
+                 "-D","PNG_LIBCONF_HEADER="+os.path.abspath("libpng/pnglibconf.h.prebuilt")
+                 ])
+            
     run_command(["cmake","--build",f"Build/libpng/{build_type}","--config",build_type])
     run_command(["cmake","--install",f"Build/libpng/{build_type}","--config",build_type])
     os.chdir(cwd)
@@ -48,7 +52,17 @@ def build_msdfgen(build_type:str):
         "-DMSDFGEN_DYNAMIC_RUNTIME=ON",
     ]
     build_lib("msdfgen",build_type,"msdfgen",options)
+def build_zlib(build_type:str):
+    options=[
+        f"-DCMAKE_BUILD_TYPE={build_type}",
+        f"-DCMAKE_INSTALL_PREFIX=Packages/{build_type}",
+        "-DZLIB_ENABLE_TESTS=OFF",
+        "-DZLIB_ENABLE_EXAMPLES=OFF",
+        "-DBUILD_SHARED_LIBS=OFF",
+    ]
+    build_lib("zlib",build_type,"zlib",options)
 os.system("chcp 65001>nul")
 build_no_dependencies("Debug")
+build_zlib("Debug")
 build_libpng("Debug")
 build_msdfgen("Debug")
